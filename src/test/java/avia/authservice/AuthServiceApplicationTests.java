3524dd65245031260bf1e987843fc91fdb0fda5f
lnksgl <alexshlyck@gmail.com>
2022-07-22 15:54:54 +0300

    init

---
+++ src/test/java/avia/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package avia.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
